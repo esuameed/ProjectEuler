9fff9f02d049a3b5af53d781d4a05751bf28fdf4
EDILSON SUAME <esuameed@gmail.com>
2022-09-13 21:52:55 -0300

    Multiples of 3 or 5

--- Perl/Problem1/Multiples.pl
+++ Perl/Problem1/Multiples.pl
@@ -2,13 +2,13 @@ use warnings;
 use strict;
 
 
-my @stack = (); # empty array
+my @stack = (); # introducing how to create an empty array in Perl!!! I know that its not necessary! But we gain some experience with arrays in Perl!!
 my $total = 0;
 
 for (my $i=1; $i < 1000; $i++){
 
 	if($i % 3==0 or $i % 5==0){
-		push(@stack,$i);
+		push(@stack,$i); # Add element to array!
 	}
 
 }
